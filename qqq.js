d1abda13a8f456075e655ba16e27818b1a081424
Don Frederico <Fred-Jones@users.noreply.github.com>
2015-10-26 23:51:14 -0600

    tried event constructor

--- qqq.js
+++ qqq.js
@@ -1,17 +1,16 @@
 var links = document.getElementsByTagName('a')
-for(var i =0; i<links.length; i++) {
-  setTimeout(inner(links), 2000)
-}
+var clickEvent = new MouseEvent('click', {
+    "view": window,
+    "bubbles": true,
+    "cancelable": false
+})
 function inner(link) {
   for(var i=0; i<link.length; i++){
-    if(link[i].name != ''){
-      link[i].click()
+    if(link[i].name != '' && !link[i].length){
+      link[i].dispatchEvent(clickEvent)
+      console.log(link[i])
     }
   }
 }
+inner(links)
 
-/* Need to handle the fact that some 'doc's are actually arrays of length 1
-described below*/
-var name = 'sample_submission.csv.zip'
-var doc = document.getElementsByName(name)
-doc[0].click()
